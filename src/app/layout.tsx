f8dbe4a49b319a16d96c8d8167abbaae9d02ad62
Dirosaki <diegosgomes27@gmail.com>
2024-12-19 22:34:13 -0300

    project configuration

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import '@/styles/global.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Busca com IA',
+  description: 'Uma página de busca que utiliza IA para encontrar resultados',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html className="dark" lang="pt-BR">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  )
+}
